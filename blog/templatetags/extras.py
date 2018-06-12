8dadc8d1eb8d8a3a53f7883272f40c7b01a83a7b
istek <i@zhoutao.ren>
2018-06-12 22:55:28 +0800

    main theme file is finished now, right now CODE!

--- blog/templatetags/extras.py
+++ blog/templatetags/extras.py
@@ -1,15 +1,19 @@
 from django import template
+import html
+from html.parser import HTMLParser
 
 register = template.Library()
 
 
 @register.filter(name='more')
 def more(value):
-
     if '<!--more-->' in value:
         nstr = '<!--more-->'
         npos = value.index(nstr)
-        nvalue = value[npos:]
-        return nvalue
+        nvalue = value[:npos]
+        html_parser = HTMLParser()
+
+        newtitle = html_parser.unescape(nvalue)
+        return newtitle
     else:
         return value
